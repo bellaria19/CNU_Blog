7ca2cb16191553906c36ce2322b5605afabac9ea
bellaria19 <current11@naver.com>
2023-06-06 00:08:12 +0900

    add description

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,5 +1,14 @@
 const Resume = () => {
-  return <div>나는 프로젝트 내역</div>;
+  return (
+    <div>
+      <h2>Real_Coding</h2>
+      <h3>2023 Semester 1</h3>
+      <h2>데이터베이스 텀프로젝트</h2>
+      <h3>Django 사용해 진행</h3>
+      <h2>개인 공부</h2>
+      <h3>Unity, C#</h3>
+    </div>
+  );
 };
 
 export default Resume;
