2a275d7bc11d7c46a4c6420284fb0f3e1e743a5f
bellaria19 <current11@naver.com>
2023-06-06 00:14:12 +0900

    description3

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -2,10 +2,15 @@ const Resume = () => {
   return (
     <div>
       <h2>Real_Coding</h2>
+      <p>2023.03 ~ 2023.06</p>
       <p>2023 Semester 1</p>
+
       <h2>데이터베이스 텀프로젝트</h2>
+      <p>2023.04 ~ 2023.06</p>
       <p>Django 사용해 진행</p>
+
       <h2>개인 공부</h2>
+      <p>2023.03 ~ </p>
       <p>Unity, C#</p>
     </div>
   );
